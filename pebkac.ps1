eff26f47f1071bcfecc10d5e3ce0e805eeb0e661
LemonScent77 <157524902+LemonScent77@users.noreply.github.com>
2024-04-11 08:48:48 -0500

    Edited Script to Pull PC name on initial run

--- pebkac.ps1
+++ pebkac.ps1
@@ -1,9 +1,33 @@
+# Function to retrieve computer name
+function Get-ComputerName {
+    $computerName = $env:COMPUTERNAME
+    return $computerName
+}
 
-$filter = @{LogName = "Security"; Id = 4625; StartTime = (Get-Date).AddDays(-5)}
+# Retrieve computer name
+$computerName = Get-ComputerName
 
-$lockouts = Get-WinEvent -ComputerName 'Name Of PC' -FilterHashTable $filter -MaxEvents 1 -ErrorAction 0
+# Set filter for Get-WinEvent
+$filter = @{
+    LogName = "Security"
+    Id = 4625
+    StartTime = (Get-Date).AddDays(-5)
+}
 
-$lockouts| Select-Object @{Name = "LockedUserName"; Expression = {$_.Properties[5].Value}}, `
-@{Name = "LogonType"; Expression = {$_.Properties[10].Value}}, `
-@{Name = "LogonProcessName"; Expression = {$_.Properties[11].Value}}, `
-@{Name = "ProcessName"; Expression = {$_.Properties[18].Value}}
+# Retrieve security events related to account lockouts
+$lockouts = Get-WinEvent -ComputerName $computerName -FilterHashTable $filter -MaxEvents 1 -ErrorAction 0
+
+# Display relevant information about the lockout event
+$lockouts | Select-Object @{
+    Name = "LockedUserName"
+    Expression = {$_.Properties[5].Value}
+}, @{
+    Name = "LogonType"
+    Expression = {$_.Properties[10].Value}
+}, @{
+    Name = "LogonProcessName"
+    Expression = {$_.Properties[11].Value}
+}, @{
+    Name = "ProcessName"
+    Expression = {$_.Properties[18].Value}
+}
